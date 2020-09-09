5ae19df64fa9d7e50bc0696984803008f84f13dd
Christoffer Ventus <christoffer.ventus@aito.ai>
2020-09-09 23:11:52 +0300

    Add simple tests for env var parsers

---
+++ test/config/variables.spec.ts
@@ -0,0 +1,21 @@
+import { boolean, number, required } from "@libs/server/config"
+
+describe('parseVariables', () => {
+  describe('boolean', () => {
+    it('should throw exception if casting wrong type to boolean', () => {
+      expect(() => boolean('1234')).toThrowError(/Non-boolean value found/)
+    })
+  })
+
+  describe('number', () => {
+    it('should throw exception if casting wrong type to number', () => {
+      expect(() => number('a string value')).toThrowError(/Non-number value found/)
+    })
+  })
+
+  describe('required', () => {
+    it('should throw exception if casting wrong type to number', () => {
+      expect(() => required(number)(undefined)).toThrowError(/environment variable is not set properly/)
+    })
+  })
+})
